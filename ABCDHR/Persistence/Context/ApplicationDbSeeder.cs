b8b53e437c6abbb591790b1d0abdd2b56a60eea7
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-06-10 06:57:52 +0100

    seeder

---
+++ ABCDHR/Persistence/Context/ApplicationDbSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class ApplicationDbSeeder
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+
+    }
+}
